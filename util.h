851540e180dc37a6c85d3db5e9867d4c4dfb8fac
tanxunrong <tanxrong@gmail.com>
2014-08-14 22:38:41 +0800

    jade add sample & cmake file

---
+++ util.h
@@ -0,0 +1,14 @@
+
+#ifndef JADE_UTIL_H
+#define JADE_UTIL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <ctype.h>
+
+static int line_num = 0;
+void inc_line_num();
+int get_line_num();
+
+#endif
